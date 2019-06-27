27a4f2270ab2f38c00ba8d611c8f9277a7904579
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-27 00:49:33 -0600

    pointing to firebase

---
+++ assets/js/schedule.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCVq4oQ_wAsrvNmep04IIo2xOtL24r9DsA",
+    authDomain: "uofu0618.firebaseapp.com",
+    databaseURL: "https://uofu0618.firebaseio.com",
+    projectId: "uofu0618",
+    storageBucket: "uofu0618.appspot.com",
+    messagingSenderId: "120030325320",
+    appId: "1:120030325320:web:aaa7ee651811d9bb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  let database = firebase.database();
+
+  
